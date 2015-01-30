932b2a4fb5fcf2e7960ec003b22f1834c859acf9
Ivanner Mora <ivanner@ivanner.com>
2015-01-30 17:21:40 -0600

    Initial commit

---
+++ components/global (2).js
@@ -0,0 +1,25 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 6:21:35 Dec 26, 2014 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 15:13:39 Jan 23, 2015.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+$.noConflict();
+jQuery(document).ready(function($) {
+	
+	$("a#nav-link-3, a#nav-link-199").click(function(e){
+		e.preventDefault();		
+	})
+	//$("#lang-switch ")
+	
+	$("a#nav-link-368, a#nav-link-369").click(function(e){
+		e.preventDefault();		
+	})
+});
